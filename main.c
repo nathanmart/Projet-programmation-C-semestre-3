d360515d480f518bfcf97311d07bba6ce53bdcb5
Projet <nathanmart95@gmail.com>
2022-10-20 14:25:52 +0200

    Modification des structures

--- main.c
+++ main.c
@@ -1,9 +1,51 @@
+//Nathan MARTORANA
+//Pierre BOTHUA
+//
+//Projet GIF
+//Automne 2022
+
 #include <stdio.h>
 
+//Structure des villes
+typedef struct ville {
+    int codePostal;
+    int nom;
+    struct ville * villeSuivante;
+} Tville;
+
+typedef Tville * PTville;
+
+
+//Structure des lignes electriques
+typedef struct lignesElectrique {
+    int puissance;
+    // pointeur sur une ville
+    PTville villeDesservie;
+    // liste simplement chainée
+    struct lignesElectrique * ligneSuivante ;
+} TlignesElectrique;
+
+typedef TlignesElectrique * PTligneElectrique;
+
+
+//Structure des centrales electriques
+typedef struct centrale{
+    int codeCentrale;
+    int puissance_max;
+    int puissance_restante;
+    // Pointeur sur la liste des lignes
+    PTligneElectrique villeDependante;
+    // Liste doublement chainée
+    struct centrale * ptsuivant;
+    struct centrale * ptprecedent;
+}Tcentrale;
+
+typedef Tcentrale * PTcentrale;
+
+
 int main() {
-    printf("Hello, World!\n");
-    printf("Modification numero 1");
-    printf("Modificatio numero 2");
+
+
 
     return 0;
 }
